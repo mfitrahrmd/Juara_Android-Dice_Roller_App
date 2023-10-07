c029ff5f78f2d6d4f5c328162cfb9474c9ead041
mfitrahrmd <tgcfitrah26@gmail.com>
2023-10-07 13:53:13 +0700

    add an image

--- app/src/main/java/com/mfitrahrmd/juaraandroid/MainActivity.kt
+++ app/src/main/java/com/mfitrahrmd/juaraandroid/MainActivity.kt
@@ -3,8 +3,11 @@ package com.mfitrahrmd.juaraandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -13,8 +16,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mfitrahrmd.juaraandroid.ui.theme.JuaraAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,6 +49,11 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.dice_1),
+            contentDescription = "1"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = { /*TODO*/ }) {
             Text(text = stringResource(id = R.string.roll))
         }
